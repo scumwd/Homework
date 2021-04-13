6d50517a3afa464c345db38dfebf24b729cfa398
scumwd <80896130+scumwd@users.noreply.github.com>
2021-04-13 15:41:02 +0300

    Add files via upload

---
+++ kr13.04(1)/kr13.04(1)/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kr13._04_1_
+{
+    class Program
+    {
+        static void Main()
+        {
+            SetOfStacks<string> t = new SetOfStacks<string>(20);
+            t.Push("fdsfd");
+
+
+        }
+    }
+    class SetOfStacks<T>
+    {
+        readonly int _capacity;
+        SetOfStacks<T> _previous;
+        Node _head;
+        int _count;
+
+        public SetOfStacks(int capacity)
+        {
+            _capacity = capacity;
+        }
+
+        public SetOfStacks(SetOfStacks<T> stack)
+        {
+            _capacity = stack._capacity;
+            _previous = stack._previous;
+            _count = stack._count;
+            _head = stack._head;
+        }
+
+        public void Push(T value)
+        {
+            if (_count >= _capacity)
+            {
+                _previous = new SetOfStacks<T>(this);
+                _count = 0;
+                _head = null;
+            }
+            var node = new Node(value);
+            if (_head == null)
+            {
+                _head = node;
+            }
+            else
+            {
+                node.Next = _head;
+                _head = node;
+            }
+            _count++;
+        }
+        public T PopAt(int index)
+        {
+            if (index == 0)
+            {
+                return Pop();
+            }
+            SetOfStacks<T> cursor = _previous;
+            while (index-- > 0)
+            {
+                if (cursor._previous == null)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                cursor = cursor._previous;
+            }
+            return cursor.Pop();
+        }
+
+        public T Pop()
+        {
+            while (_count == 0)
+            {
+                if (_previous == null)
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+                _count = _previous._count;
+                _head = _previous._head;
+                _previous = _previous._previous;
+            }
+
+            var value = _head.Value;
+            _head = _head.Next;
+            _count--;
+            return value;
+        }
+
+        class Node
+        {
+            public Node(T value)
+            {
+                Value = value;
+            }
+            public Node Next;
+            public T Value;
+        }
+    }
+}
---
+++ kr13.04(2)/kr13.04(2)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+//Ибнеев Артем 11-012
+namespace kr13._04_2_
+{
+    class Program
+    {
+        public static Random rnd = new();
+        static void Main()
+        {
+            int n = 10;            
+            List<double> num = new List<double>();
+            for(int i = 0; i < n; i++)
+            {
+                num.Add(rnd.NextDouble()*50);
+            }
+            foreach (double item in num)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            List<double> num1 = new();
+            for(int i = 0; i < n; i++)
+            {
+                num1.Add(Sum(num, i));
+            }
+            foreach (double item in num1)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static double Sum(List<double> num, int i)
+        {
+            double sum = 0;
+            for (int j = 0; j < i + 1; j++)
+                sum += num[j];
+            return sum;
+        }
+    }
+}
